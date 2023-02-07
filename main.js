cb9de5866986e01e7f6e9a8f502fe7faa7b32ab6
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-06 21:11:24 -0300

    Aplicando eventos e callbacks

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
 let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-function handleClick(event) {
-  event.preventDefault()
+function handleTryClick(e) {
+  e.preventDefault()
   const inputNumber = document.querySelector('#inputNumber')
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector('.screen1').classList.add('hide')
-    document.querySelector('.screen2').classList.remove('hide')
+    screen1.classList.add('hide')
+    screen2.classList.remove('hide')
 
     document.querySelector(
       '.screen2 h2'
     ).innerText = `Você acertou em ${xAttempts} tentativas`
   }
 
+  inputNumber.value = ''
+
   xAttempts++
 }
+
+// Events
+
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', () => {
+  screen1.classList.remove('hide')
+  screen2.classList.add('hide')
+  xAttempts = 1
+})
