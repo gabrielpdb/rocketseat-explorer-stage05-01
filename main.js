545e7aac955a7af95b2babe16cbd07fc23568b4d
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-06 21:37:46 -0300

    Refatorando e algumas ideias a mais

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+const inputNumber = document.querySelector('#inputNumber')
 let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
+// Events
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
+})
+
+// Callbacks
 function handleTryClick(e) {
   e.preventDefault()
-  const inputNumber = document.querySelector('#inputNumber')
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add('hide')
-    screen2.classList.remove('hide')
-
-    document.querySelector(
-      '.screen2 h2'
+    toggleScreen()
+    screen2.querySelector(
+      'h2'
     ).innerText = `Você acertou em ${xAttempts} tentativas`
   }
 
@@ -21,14 +31,14 @@ function handleTryClick(e) {
   xAttempts++
 }
 
-// Events
+function handleResetClick() {
+  toggleScreen()
+  inputNumber.focus()
+  // randomNumber = Math.round(Math.random() * 10)
+  xAttempts = 0
+}
 
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
-
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', () => {
-  screen1.classList.remove('hide')
-  screen2.classList.add('hide')
-  xAttempts = 1
-})
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
